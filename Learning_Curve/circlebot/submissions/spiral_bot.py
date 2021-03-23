ebdbf374f54e0f54104d4e15ec3398ba0044998b
Shashank <hsshashank9@gmail.com>
2021-03-23 16:55:12 +0530

    Added circlebot

---
+++ Learning_Curve/circlebot/submissions/spiral_bot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def circle():
+    rospy.init_node('circle_node', anonymous=True)
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    velocity = Twist()
+    rate = rospy.Rate(10) #10Hz
+    rospy.loginfo("Moving the bot")
+    radius = 0.1
+
+    while not rospy.is_shutdown():
+        velocity.linear.x = 1.0
+        velocity.angular.z = 1.0/radius
+        radius+= 0.01
+        pub.publish(velocity)
+        rate.sleep()
+    
+    velocity.linear.x = 0
+    velocity.angular.z = 0
+    pub.publish(velocity)
+
+if __name__=='__main__':
+    try:
+        circle()
+    except rospy.ROSInterruptException:
+        pass
